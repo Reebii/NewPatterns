7b131ba774536c84f7285fc8210a5ce438f6664f
RAJIV DEY <deyrajiv2572@gmail.com>
2025-02-24 15:53:37 +0530

    Update ytDrawshort.py

--- ytDrawshort.py
+++ ytDrawshort.py
@@ -1,27 +1,38 @@
-from turtle import*
+from turtle import *
 import colorsys
+
 bgcolor("black")
-tracer(500)
+tracer(10)  # Adjusted for better performance
 
 def draw():
-    h=0
+    h = 0
+
     for i in range(75):
-        c=colorsys.hsv_to_rgb(h,1,1)
-        h+=0.5
+        c = colorsys.hsv_to_rgb(h, 1, 1)   
+        c = "#{:02x}{:02x}{:02x}".format(
+            int(c[0] * 255), int(c[1] * 255), int(c[2] * 255)
+        )   
+
+        h += 0.02   
+
         up()
-        goto(0,0)
+        goto(0, 0)
         down()
+
         color("black")
-        fillcolor(c)
+        fillcolor(c)   
+
         begin_fill()
         rt(98)
-        circle(i,12)
-        fd(290)
-        fd(i)
+        circle(i, 12)
+        fd(150)  #  kam hai ab spacing
         lt(29)
-        for j in range(50):
-            fd(i)
-            circle(j,299,steps=2)
+
+        for j in range(20):  #  yeh loop circle ko adjusted rakhega
+            fd(i // 2)
+            circle(j, 180, steps=2)
+
         end_fill()
+
 draw()
-done()            
+done()
